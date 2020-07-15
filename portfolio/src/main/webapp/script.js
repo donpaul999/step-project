70e562b8daa1505673a25575f5a696db002816f4
Paul Colta <coltapaulstefan@gmail.com>
2020-07-15 11:32:05 +0000

    Feature UserLogIn - Week 4

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
+
+ /** Adds a random greeting to the page.
 
 function addRandomGreeting() {
   const greetings =
