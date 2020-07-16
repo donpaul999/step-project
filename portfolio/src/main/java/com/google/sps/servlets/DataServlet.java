3ac4cac7de78553c73d07ae7285bb660ddaac3a0
Paul Colta <coltapaulstefan@gmail.com>
2020-07-16 06:28:02 +0000

    Revert "Add comment to DataServlet Post function"
    This reverts commit a94449c69ea2f536ecf67d0fd618070f7fbe74c4.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -43,11 +43,6 @@ public class DataServlet extends HttpServlet {
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
-
-
-  /**
-  * Gets message from user and saves it into the database
-  */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String newMessage = getParameter(request, "text-input", "");
