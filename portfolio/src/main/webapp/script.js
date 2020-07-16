7743f4e9d7cd2a719e4a4523de9a838b24b2c2fb
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2020-07-16 15:34:27 +0300

    Update script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,128 +12,101 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
-
-function addRandomGreeting() {
-  const greetings =
-      ['Buna ziua!', 'Neata!', 'Buna seara！'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
-
-async function getRandomNameUsingAsync() {
-  const response = await fetch('/data');
-  const name = await response.text();
-  document.getElementsByTagName('body').innerText = name; 
-}
-*/
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 
-function getMessagesFromServer() {
-  var numberOfComments = document.getElementById("messages-number").value;
+async function getCommentsFromServer() {
+  var numberOfComments = document.getElementById("comments-number").value;
   if(numberOfComments == null){
       numberOfComments = 0;
   }
   
   var url = '/data?nr=' + numberOfComments;
-  fetch(url).then(response => response.json()).then((messages) => {
-    const messagesListElement = document.getElementById('messages-container');
+  return fetch(url).then(response =>response.json());
+  
+}
+
+
+async function handleGetCommentsClick(){
+   const messages = await getCommentsFromServer();
+   renderComments(messages);
+}
+
+async function renderComments(messages){
+    console.log("2:" + messages);
+    const messagesListElement = document.getElementById('comments-container');
     if(messagesListElement.innerHTML !== ''){
         messagesListElement.innerHTML = '';
     }
 
-    for(i = 0; i < messages.length; ++i){
-        childToAppend = createListElement(messages[i].propertyMap.content);
-        childToAppend.appendChild(createButton(messages[i].propertyMap.messageId));
-        messagesListElement.appendChild(childToAppend);
+    for(var i = 0; i < messages.length; ++i){
+        var domListElement = createDOMListElement(messages[i].propertyMap.email, messages[i].propertyMap.content);
+        domListElement.appendChild(createDOMButton(messages[i].propertyMap.messageId));
+        messagesListElement.appendChild(domListElement);
     }
     
     if(messages.length === 0){
-        var content = "No messages available!";
-        childToAppend = createListElement(content);
-        messagesListElement.appendChild(childToAppend);
+        var content = "No comments available!";
+        var domListElement = createDOMListElement(content);
+        messagesListElement.appendChild(domListElement);
     }
-  });
 }
 
-
-function createListElement(text) {
-  const liElement = document.createElement('li');
-  liElement.innerText = text;
-  liElement.classList.add('list-group-item');
-  return liElement;
+function createDOMListElement(email, text) {
+  const domListElement = document.createElement('li');
+  if(email != "")
+   domListElement.innerText = email + ": ";
+  domListElement.innerText += text;
+  domListElement.classList.add('list-group-item');
+  return domListElement;
 }
 
-function createButton(messageId) {
-  const buttonElement = document.createElement('button');
-  buttonElement.classList.add('delete-button');
-  buttonElement.textContent = "Delete";
-  buttonElement.id = messageId;
-  buttonElement.setAttribute( "onClick", "javascript: deleteMessage(this);" );
-  return buttonElement;
+function createDOMButton(messageId) {
+  const domButtonElement = document.createElement('button');
+  domButtonElement.classList.add('delete-button');
+  domButtonElement.textContent = "Delete";
+  domButtonElement.id = messageId;
+  domButtonElement.setAttribute( "onClick", "javascript: handleDeleteCommentClick(this);" );
+  return domButtonElement;
 }
 
+async function handleDeleteCommentClick(this){
+   deleteComment(this);
+   const messages = await getCommentsFromServer();
+   renderComments(messages);
+}
 
-function deleteMessage(thisButton) {
+function deleteComment(thisButton) {
   const params = new URLSearchParams();
   var messageId = thisButton.id;
   var liElementToDelete = thisButton.parentNode;
   params.append('messageId', messageId);
-  deleteLi(liElementToDelete);
   fetch('/delete-data', {method: 'POST', body: params});
 }
 
-function deleteLi(liToDelete) {
-  var listWhereToRemove = liToDelete.parentNode;
-  listWhereToRemove.removeChild(liToDelete);
-}
 
-function showAbout() {
+function showSection(idToShow) {
   var elem1 = document.getElementById("about");
   var elem2 = document.getElementById("skills");
   var elem3 = document.getElementById("contact");
-
-  if (elem1.style.display === "none") {
-    elem1.style.display = "block";
-    elem2.style.display = "none";
-    elem3.style.display = "none";
-  } else {
-    elem1.style.display = "none";
-  }
-}
-
-function showSkills() {
-  var elem1 = document.getElementById("skills");
-  var elem2 = document.getElementById("about");
-  var elem3 = document.getElementById("contact");
-
-  if (elem1.style.display === "none") {
-    elem1.style.display = "block";
-    elem2.style.display = "none";
-    elem3.style.display = "none";
-  } else {
-    elem1.style.display = "none";
-  }
-}
-
-function showContact() {
-  var elem1 = document.getElementById("contact");
-  var elem2 = document.getElementById("skills");
-  var elem3 = document.getElementById("about");
-
-  if (elem1.style.display === "none") {
-    elem1.style.display = "block";
-    elem2.style.display = "none";
-    elem3.style.display = "none";
-  } else {
-    elem1.style.display = "none";
-  }
-
+  var elem4 = document.getElementById(idToShow);
+  var statusOfElem4 = elem4.style.display;
+  elem1.style.display = "none";
+  elem2.style.display = "none";
+  elem3.style.display = "none";
+  if (statusOfElem4 === "none") {
+    elem4.style.display = "block";
+  } 
 }
